77fc34b2327bd25cf295194e19b24977fb1ff1d7
Lee Kwenwon <highfence@naver.com>
2017-09-15 15:50:02 +0900

    [ADD] Make CharacterSelectSceneManager

---
+++ Assets/Scripts/SceneLogic/2. CharacterSelectScene/CharacterSelectSceneManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+/*
+ * Character Select Scene 관리자 클래스.
+ */
+public partial class CharacterSelectSceneManager : MonoBehaviour
+{
+    DataContainer _dataContainer;
+    NetworkManager _networkManager;
+
+    public void Awake()
+    {
+        _dataContainer = DataContainer.GetInstance() as DataContainer;
+        _networkManager = NetworkManager.GetInstance() as NetworkManager;
+
+        SubscribePacketEvents(_networkManager);
+    }
+
+    // 패킷 도착 이벤트 메소드들을 처음에 등록해주는 함수.
+    private void SubscribePacketEvents(NetworkManager network)
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/SceneLogic/2. CharacterSelectScene/CharacterSelectSceneManagerGui.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Character Select Scene의 Gui 클래스
+ */
+public partial class CharacterSelectSceneManager : MonoBehaviour
+{
+
+}
