b742d185e0675fd59a705dfb547ef9f63c37ee71
Lee Kwenwon <highfence@naver.com>
2017-09-18 20:25:01 +0900

    [REFACTORING] Not divide to Partial class for GUI

--- Assets/Scripts/SceneLogic/1. LoginScene/LoginSceneManager.cs
+++ Assets/Scripts/SceneLogic/1. LoginScene/LoginSceneManager.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 /*
  * 로그인 씬 관리자 클래스.
  */
-public partial class LoginSceneManager : MonoBehaviour
+public class LoginSceneManager : MonoBehaviour
 {
+    #region PACKET LOGIC
+
     DataContainer _dataContainer;
     NetworkManager _networkManager;
 
@@ -126,4 +129,74 @@ public partial class LoginSceneManager : MonoBehaviour
 
         return true;
     }
+
+    #endregion
+
+    #region GUI
+
+    public InputField _idField;
+    public InputField _pwField;
+    public string _id;
+    public string _pw;
+    public string _reqUserId;
+
+    private void Start()
+    {
+        #region MAKE BACKGROUND
+
+        var bgPrefab = Resources.Load("Prefabs/Background") as GameObject;
+        var bgInstance = Instantiate(bgPrefab).GetComponent<Background>();
+
+        #endregion
+
+        #region INITIALIZE INPUT FIELDS
+
+        _idField.text = "ID";
+        _pwField.text = "Password";
+
+        var idEvent = new InputField.SubmitEvent();
+        idEvent.AddListener(GetId);
+        _idField.onEndEdit = idEvent;
+
+        var pwEvent = new InputField.SubmitEvent();
+        pwEvent.AddListener(GetPw);
+        _pwField.onEndEdit = pwEvent;
+
+        #endregion
+    }
+
+    private void OnGUI()
+    {
+        ButtonCheck();
+    }
+
+    // 버튼이 눌렸는지 체크해주는 메소드.
+    private void ButtonCheck()
+    {
+        if (GUI.Button(new Rect((Screen.width / 2) - 155, Screen.height * 2 / 3, 150, 50), "Login"))
+        {
+            TryLogin(_id, _pw);
+        }
+
+        // TODO :: 이걸 회원가입 버튼으로 바꿔야 될 것 같은데?
+        if (GUI.Button(new Rect((Screen.width / 2) + 5, Screen.height * 2 / 3, 150, 50), "Exit"))
+        {
+            Application.Quit();
+        }
+    }
+
+    // Input Field에 적힌 string을 가져오는 콜백 메소드.
+    #region INPUT FIELD CALL BACK
+    private void GetId(string arg0)
+    {
+        _id = arg0;
+    }
+
+    private void GetPw(string arg0)
+    {
+        _pw = arg0;
+    }
+    #endregion
+
+    #endregion
 }
--- Assets/Scripts/SceneLogic/3. MatchingScene/MatchSceneManager.cs
+++ Assets/Scripts/SceneLogic/3. MatchingScene/MatchSceneManager.cs
@@ -5,9 +5,10 @@ using UnityEngine.SceneManagement;
 /*
  * 매칭 씬을 담당하는 클래스.
  */
-public partial class MatchSceneManager : MonoBehaviour
+public class MatchSceneManager : MonoBehaviour
 {
-    DataContainer  _dataContainer  = null;
+    #region PACKET LOGIC
+    DataContainer _dataContainer = null;
     NetworkManager _networkManager = null;
 
     private void Start()
@@ -53,8 +54,8 @@ public partial class MatchSceneManager : MonoBehaviour
     // 매치 취소 요청 답변 패킷 처리.
     private void OnMatchCancelRes(PacketInfo.MatchCancelRes receivedPacket)
     {
-       // TODO :: OnFastmatchRes와 같은 고려 필요. 
-       if (receivedPacket._result == (int)ErrorCode.None)
+        // TODO :: OnFastmatchRes와 같은 고려 필요. 
+        if (receivedPacket._result == (int)ErrorCode.None)
         {
             return;
         }
@@ -73,7 +74,7 @@ public partial class MatchSceneManager : MonoBehaviour
 
         // 게임 씬으로 상태를 바꾼다.
         // TODO :: 게임 확인 패킷이 날라올때 까지의 컷씬이 필요하지 않을까 싶기도.
-        SceneManager.LoadScene("Game"); 
+        SceneManager.LoadScene("Game");
     }
 
     // 매치 요청 패킷을 보낸다.
@@ -111,4 +112,118 @@ public partial class MatchSceneManager : MonoBehaviour
 
         _networkManager.SendPacket<PacketInfo.MatchSuccessAck>(successAck, PacketInfo.PacketId.ID_MatchSuccessAck);
     }
+
+    #endregion
+
+    #region GUI
+
+    private GameObject _curtain = null;
+    private GUIStyle _textStyle = null;
+    private GameObject _modelInstance = null;
+    private bool _isTryingNumberMatch = false;
+    private bool _isTryingFastMatch = false;
+    private float _matchingCountTime = 0.0f;
+    private string[] _matchingString;
+    private int _matchingCountIdx = 0;
+
+    private void Awake()
+    {
+        SetTextOption();
+        MakeAnim();
+    }
+
+    private void OnGUI()
+    {
+        MakeButton();
+    }
+
+    private void MakeAnim()
+    {
+        var selectInfo = FindObjectOfType<PlayerInfo>();
+
+        if (selectInfo == null) return;
+
+        string prefabPath = "PrivateData/SpritesArchers/FantasyArcher_0" + (int)selectInfo._selectedPlayerType;
+
+        _modelInstance = Instantiate(Resources.Load(prefabPath) as GameObject);
+
+        _modelInstance.GetComponent<Transform>().position = new Vector3(0f, 1f, 0f);
+    }
+
+    private void MakeButton()
+    {
+        if (_isTryingFastMatch == false && _isTryingNumberMatch == false)
+        {
+            if (GUI.Button(new Rect((Screen.width / 2) - 155, (Screen.height * 2 / 3), 150, 150), "Fast Match"))
+            {
+                _isTryingFastMatch = true;
+
+                SendMatchReqPacket();
+                _curtain.GetComponent<BlackCurtain>().StartFadeIn();
+            }
+            if (GUI.Button(new Rect((Screen.width / 2) + 5, (Screen.height * 2 / 3), 150, 150), "Number Match"))
+            {
+                // TODO :: Number Match는 추후에 구현.
+                _curtain.GetComponent<BlackCurtain>().StartFadeIn();
+
+                _isTryingNumberMatch = true;
+            }
+        }
+        else if (_isTryingNumberMatch == true)
+        {
+            TryNumberMatch();
+        }
+        else
+        {
+            TryFastMatch();
+        }
+    }
+
+    // 텍스트 옵션을 세팅해주는 함수.
+    private void SetTextOption()
+    {
+        _textStyle = new GUIStyle();
+        _textStyle.fontSize = 40;
+        _textStyle.normal.textColor = Color.white;
+
+        _matchingString = new String[3];
+        _matchingString[0] = "Matching.";
+        _matchingString[1] = "Matching..";
+        _matchingString[2] = "Matching...";
+    }
+
+    private void TryFastMatch()
+    {
+        MakeMatchWaitingScene();
+    }
+
+    private void TryNumberMatch()
+    {
+        MakeMatchWaitingScene();
+    }
+
+    private void MakeMatchWaitingScene()
+    {
+        _modelInstance.GetComponent<Animator>().enabled = false;
+        _matchingCountTime += Time.deltaTime;
+
+        if (_matchingCountTime >= 1.0f)
+        {
+            _matchingCountTime = 0f;
+            ++_matchingCountIdx;
+            if (_matchingCountIdx == 3) _matchingCountIdx = 0;
+        }
+
+        GUI.Label(new Rect((Screen.width / 2) - 100, Screen.height / 3, 200, 100), _matchingString[_matchingCountIdx], _textStyle);
+
+        if (GUI.Button(new Rect((Screen.width / 2) - 100, Screen.height * 2 / 3, 200, 100), "Cancel"))
+        {
+            _curtain.GetComponent<BlackCurtain>().StartFadeOut();
+            _isTryingNumberMatch = false;
+            _isTryingFastMatch = false;
+            _modelInstance.GetComponent<Animator>().enabled = true;
+        }
+    }
+
+    #endregion
 }
