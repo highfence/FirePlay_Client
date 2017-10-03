a94b7d7803f5fd27d43e40667c39dba6c7d267b8
Lee Kwenwon <highfence@naver.com>
2017-10-03 21:39:18 +0900

    [ADD] EffectManager

--- Assets/Scripts/Prefabs/Explosion.cs
+++ Assets/Scripts/Prefabs/Explosion.cs
@@ -1,10 +1,56 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ExplosionType
+{
+    Type1,
+    Type2,
+    Type3
+}
+
 public class Explosion : MonoBehaviour
 {
-    private void Init()
+    private Dictionary<ExplosionType, string> _explosionTypeToSpecPath = new Dictionary<ExplosionType, string>()
     {
+        { ExplosionType.Type1, "Data/Type1" },
+        { ExplosionType.Type2, "Data/Type2" },
+        { ExplosionType.Type3, "Data/Type3" }
+    };
+
+    private void Init(ExplosionType type)
+    {
+
+    }
+
+    private struct ExplosionSpec
+    {
+        [SerializeField]
+        float _range;
+
+        [SerializeField]
+        int _minDamage;
+        int _maxDamage;
+
+        [SerializeField]
+        string _spritePath;
+
+        public static ExplosionSpec CreateFromText(string text)
+        {
+            ExplosionSpec instance;
+            try
+            {
+                instance = JsonUtility.FromJson<ExplosionSpec>(text);
+            }
+            catch (Exception e)
+            {
+                Debug.LogErrorFormat("[ExplosionSpec] Cannot make instance from text - {0}, {1}", text, e.Message);
+                throw;
+            }
+
+            return instance;
+        }
     }
 }
+
---
+++ Assets/Scripts/Utils/EffectManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectManager : MonoBehaviour
+{
+    public GameObject[] _effects;
+
+    #region SINGLETON
+
+    private static EffectManager _instance = null;
+
+    private void Awake()
+    {
+        Initialize();
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Initialize()
+    {
+
+    }
+
+    public static EffectManager GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = Instantiate(Resources.Load("Prefbas/EffectManager") as GameObject).GetComponent<EffectManager>;
+        }
+
+        return _instance;
+    }
+
+    #endregion
+}
