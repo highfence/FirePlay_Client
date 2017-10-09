cd62182d602f3bb4c6403049285f2548bc0d406a
Lee Kwenwon <highfence@naver.com>
2017-10-10 05:21:41 +0900

    [ADD] 배경 만들기

---
+++ Assets/Scripts/Utils/BackgroundCamera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundCamera : MonoBehaviour
+{
+    Camera _mainCam;
+
+    private void Awake()
+    {
+        _mainCam = Camera.main; 
+    }
+
+    void Update ()
+    {
+        var mainCameraPos = _mainCam.transform.position;
+        mainCameraPos.z = -27;
+        this.transform.position = mainCameraPos;
+	}
+}
